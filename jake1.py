657a0b3720a1c64b26d20ae0bf55883efef6a3d5
mgharaibehAE <146516038+mgharaibehAE@users.noreply.github.com>
2025-02-28 01:07:47 -0600

    jake1.py

--- jake1.py
+++ jake1.py
@@ -94,7 +94,7 @@ class MyStResponseParser(ResponseParser):
                 st.write("Executing plot code:")
                 st.code(plot_code_clean)
                 exec(plot_code_clean, {}, local_env)
-                fig.set_size_inches(12, 8)
+                fig.set_size_inches(6, 4)
                 fig = plt.gcf()
                 if fig.axes:
                     st.pyplot(fig)
